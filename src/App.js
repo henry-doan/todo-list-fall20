f5e819ef6d0736d8455611c727bb59b20cb8be64
Henry Doan <henrydoan96@gmail.com>
2020-09-21 19:57:05 -0600

    list out all in app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { todos: [
+    { id: 1, title: "Learn Rails", complete: true },
+    { id: 2, title: "Learn React", complete: false },
+    { id: 3, title: "Learn Router", complete: false },
+  ]}
+
+  renderTodos = () => {
+    const { todos } = this.state
+    return todos.map( todo => {
+      return ( 
+        <li key={todo.id}>{todo.title}</li>
+      )
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <ul>
+          { this.renderTodos() }
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
